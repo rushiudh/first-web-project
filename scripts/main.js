97648612e113a51fd6a95bcf6855ea07a89f3796
rushiudhoji <rushikeshudhoji880@gmail.com>
2020-10-23 15:40:01 +0530

    Update 1 putting comments to understand code

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,31 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+myImage.onclick = function() { //code Starts
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/cat.jpg') {
       myImage.setAttribute('src','images/cat1.jpg');
     } else {
       myImage.setAttribute('src','images/cat.jpg');
     }
-}
-let myButton = document.querySelector('button');
+} //till now the code is about changing images
+let myButton = document.querySelector('button');//code starts
 let myHeading = document.querySelector('h1');
 function setUserName()
 {
   let myName = prompt('Enter your name.');
   localStorage.setItem('name',myName);
-  myHeading.textContent = 'Cats Are Cool' + myName;
+  myHeading.textContent = 'Cats Are Cool ' + myName;
 }
+if(!localStorage.getItem('name'))
+{
+  setUserName();
+}
+else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Cats Are Cool '+ storedName;
+}
+myButton.onclick = function()
+{
+  setUserName();
+}//at end the code selects button created in html and assign the change of
+//detail to heading called h1
